bc6589ba2eaf97f4573c62b953d92ef04ec5e96d
اسمك <email@example.com>
2025-11-30 12:52:20 +0100

    Uploadthing

---
+++ api/uploadthing/core.ts
@@ -0,0 +1,22 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { auth } from "@clerk/nextjs/server";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  storyImage: f({
+    image: {
+      maxFileSize: "4MB",
+      maxFileCount: 1,
+    },
+  })
+    .middleware(async () => {
+      const { userId } = await auth();
+      if (!userId) throw new Error("Unauthorized");
+
+      return { userId };
+    })
+    .onUploadComplete(() => {}),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
